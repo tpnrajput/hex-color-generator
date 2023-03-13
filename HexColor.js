ed034ffaad09e823bf90308531399ca4a691ab95
Tapan Jadav <125344544+tpnrajput@users.noreply.github.com>
2023-03-13 13:17:40 +0530

    Add files via upload

---
+++ HexColor.js
@@ -0,0 +1,6 @@
+function myColor(){
+    var color = document.getElementById('colorPanel').value;
+    document.body.style.backgroundColor = color;
+    document.getElementById('box').value = color;
+}
+
